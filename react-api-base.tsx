923ec697ecde0ac26ce400f04e495ef4856b960a
Kishore Kumar <kishore.jannarapu@gmail.com>
2023-12-07 10:47:05 +0800

    Update react-api-base.tsx

--- react-api-base.tsx
+++ react-api-base.tsx
@@ -29,3 +29,74 @@ function App() {
 }
 
 export default App;
+
+import React, { useContext } from 'react';
+
+function ErrorModal() {
+  const { error, clearError } = useContext(ErrorContext);
+
+  if (!error) return null;
+
+  return (
+    <div className="error-modal">
+      <h3>Error!</h3>
+      <p>{error.message}</p>
+      <button onClick={clearError}>Close</button>
+    </div>
+  );
+}
+
+export default ErrorModal;
+
+
+import React, { useContext } from 'react';
+
+function YourComponent() {
+  const { error, setError } = useContext(ErrorContext);
+
+  const handleApiCall = async () => {
+    try {
+      const response = await fetch('https://your-api.com/endpoint');
+      // Process response
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleApiCall}>Make API Call</button>
+      {error && <div>Error: {error.message}</div>}
+      {/* Your component content */}
+    </div>
+  );
+}
+
+export default YourComponent;
+
+
+import axios from 'axios';
+
+// Get base API URL from environment variable
+const BASE_URL = process.env.REACT_APP_API_URL;
+
+// Define default headers
+const defaultHeaders = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
+// Function to add authorization header
+const addAuthHeader = (token) => {
+  defaultHeaders.Authorization = `Bearer ${token}`;
+};
+
+// Create an instance of Axios configured with base URL and headers
+const apiClient = axios.create({
+  baseURL: BASE_URL,
+  headers: defaultHeaders,
+});
+
+// Export the API client and base config object
+export { apiClient, BASE_URL, addAuthHeader };
+
